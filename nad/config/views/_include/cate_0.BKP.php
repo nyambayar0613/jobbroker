bb94691c1691970e7f05cd502b789f17574e2734
maralgua <bmaral0926@gmail.com>
2021-06-03 16:58:23 -0700

    орчуулга хийгдсэн.

--- nad/config/views/_include/cate_0.BKP.php
+++ nad/config/views/_include/cate_0.BKP.php
@@ -1,14 +1,14 @@
 <?php if (!defined("_ALICE_")) exit; // 개별 페이지 접근 불가 ?>
 
 <dl class="wbg" style="padding:0 5px 5px;margin:3px"> 
-  <dl class="b nanum fon13 pd7">1үе <?php echo ($type=='job_college')?'Их ёургуулийн байршил':$type_arr[$type]['sub_name'];?> Төрөл</dl>
+  <dl class="b nanum fon13 pd7">1차 <?php echo ($type=='job_college')?'대학지역':$type_arr[$type]['sub_name'];?> 분류</dl>
   <table width="100%" cellspacing="1" class="ln sep tf">
     <col width=25><?php if($is_job){ ?><col width=30><?php } ?><col><col width=75>
     <tr align="center" class="bg b" height="25">
-      <td class="fon11" title='Хэвлэх эсэх'>V</td>
-      <?php if($is_job){ ?><td class="fon11" title="Хэрэв та том хүн ангилал сонгосон бол категорийг ашиглахдаа баталгаажуулах шаардлагатай.">Том хүн</td><?php } ?>
-      <td class="fon11">Утга</td>
-      <td class="fon11">Эмхтгэл</td>
+      <td class="fon11" title='출력유무'>V</td>
+      <?php if($is_job){ ?><td class="fon11" title="성인분류로 체크하시면 카테고리 사용시 성인인증을 요구합니다.">성인</td><?php } ?>
+      <td class="fon11">항목</td>
+      <td class="fon11">편집</td>
     </tr>
 
 	<tbody id="cate_0_list">
@@ -24,15 +24,15 @@
 			<input type='text' name="name[]" class="txt w100 cate_0_list" style="ime-mode:active;" value="<?php echo $name;?>" id="name_<?php echo $val['no'];?>" no="<?php echo $val['no'];?>" cate="cate_0"/>
 		</td>
 		<td>
-			<a class='btn'><h1 class="btn19" style="width:21px" onclick="cate_update('cate_0','<?php echo $val['no'];?>');">засварлах</h1></a>
-			<a class='btn'><h1 class="btn19" style="width:21px" onclick="cate_delete('cate_0','<?php echo $val['no'];?>');">устгах</h1></a>
+			<a class='btn'><h1 class="btn19" style="width:21px" onclick="cate_update('cate_0','<?php echo $val['no'];?>');">수정</h1></a>
+			<a class='btn'><h1 class="btn19" style="width:21px" onclick="cate_delete('cate_0','<?php echo $val['no'];?>');">삭제</h1></a>
 		</td>
 	</tr>
 	<?php 
 			}	// foreach end.
 		} else { ?>
 	<tr class="wbg tc">
-		<td colspan='<?php echo ($is_job)?'4':'3';?>' style="height:120px;">1үе <?php echo $type_arr[$type]['sub_name'];?>үүсгэнэ үү.</td>
+		<td colspan='<?php echo ($is_job)?'4':'3';?>' style="height:120px;">1차 <?php echo $type_arr[$type]['sub_name'];?>을(를) 생성해 주세요.</td>
 	</tr>
 	<?php } // if end. ?>
 	</tbody>
@@ -46,13 +46,13 @@
 
 	<tr class="bg_col tc" height="30" id="cate_0_input" style="display:none;">
 		<td><input name="view" type="checkbox" value="yes" checked></td>
-		<?php if($is_job){ ?><td><input name="etc_0" type="checkbox" value="1" id="cate_0_adult" title="Хэрэв та том хүн ангилал сонгосон бол категорийг ашиглахдаа баталгаажуулах шаардлагатай."></td><?php } ?>
+		<?php if($is_job){ ?><td><input name="etc_0" type="checkbox" value="1" id="cate_0_adult" title="성인분류로 체크하시면 카테고리 사용시 성인인증을 요구합니다."></td><?php } ?>
 		<td class="plr5">
-			<input type='text' name="name" class="txt w100 cate_name" style="ime-mode:active;" id="cate_0_name" hname='1-р үеийн ангиллын нэр' required cate="cate_0">
+			<input type='text' name="name" class="txt w100 cate_name" style="ime-mode:active;" id="cate_0_name" hname='1차 분류명' required cate="cate_0">
 		</td>
 		<td>
-			<a class='btn' onclick="cate_insert('cate_0');"><h1 class="btn19" style="width:21px">Бүртгэх</h1></a>
-			<a class='btn' onclick="cate_cancel('cate_0');"><h1 class="btn19" style="width:21px">Цуцлах</h1></a>
+			<a class='btn' onclick="cate_insert('cate_0');"><h1 class="btn19" style="width:21px">등록</h1></a>
+			<a class='btn' onclick="cate_cancel('cate_0');"><h1 class="btn19" style="width:21px">취소</h1></a>
 		</td>
 	</tr> 
 
@@ -62,11 +62,11 @@
 </dl>
   
 <dl class="bg_col tl psr" style="margin:5px 5px 0;height:25px">
-	<a class="cbtn gr2_org lnb2_org" onclick="change_ranks('cate_0','up');"><h1 class="btn19 org">▲ Дээшээ</h1></a>
-	<a class="cbtn gr2_org lnb2_org" onclick="change_ranks('cate_0','down');"><h1 class="btn19 org">▼ Доошоо</h1></a>
-	<a class="cbtn gr2_col lnb2_col" onclick="change_ranks('cate_0','first');"><h1 class="btn19 col">▲ Хамгийн эхэнд</h1></a>
-	<a class="cbtn gr2_col lnb2_col" onclick="change_ranks('cate_0','last');"><h1 class="btn19 col">▼ Сүүлд</h1></a>
+	<a class="cbtn gr2_org lnb2_org" onclick="change_ranks('cate_0','up');"><h1 class="btn19 org">▲ 위 &nbsp; 로</h1></a>
+	<a class="cbtn gr2_org lnb2_org" onclick="change_ranks('cate_0','down');"><h1 class="btn19 org">▼ 아래로</h1></a>
+	<a class="cbtn gr2_col lnb2_col" onclick="change_ranks('cate_0','first');"><h1 class="btn19 col">▲ 맨처음</h1></a>
+	<a class="cbtn gr2_col lnb2_col" onclick="change_ranks('cate_0','last');"><h1 class="btn19 col">▼ 끝으로</h1></a>
 	<dd class="prt">
-		<a class="cbtn grf_col lnb_col" onclick="cate_input('cate_0');"><h1 class="btn19"><strong>+</strong>Нэмэх</h1></a>
+		<a class="cbtn grf_col lnb_col" onclick="cate_input('cate_0');"><h1 class="btn19"><strong>+</strong>추가</h1></a>
 	</dd>
 </dl>
