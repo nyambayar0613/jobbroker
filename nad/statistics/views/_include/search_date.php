3abac4b8426879a05725662b3faeb2fdd547db99
maralgua <bmaral0926@gmail.com>
2021-06-11 15:53:47 -0700

    орчуулга хийгдсэн.

--- nad/statistics/views/_include/search_date.php
+++ nad/statistics/views/_include/search_date.php
@@ -10,17 +10,17 @@
 	<dl class="tc pd7 wbg">
 		<input name="start_day" type="text" class="tday" id='start_day' value="<?php echo ($_GET['start_day'])?$_GET['start_day']:$first_day;?>"> ~
 		<input name="end_day" type="text" class="tday" id='end_day' value="<?php echo ($_GET['start_day'])?$_GET['end_day']:$to_day;?>">
-		<a class="bbtn set_day" date='today'><h1 class="btn19">오늘</h1></a>
-		<a class="bbtn set_day" date='week'><h1 class="btn19">이번주</h1></a>
-		<a class="bbtn set_day" date='month'><h1 class="btn19">이번달</h1></a>
-		<a class="bbtn set_day" date='7day'><h1 class="btn19">1주일</h1></a>
-		<a class="bbtn set_day" date='15day'><h1 class="btn19">15일</h1></a>
-		<a class="bbtn set_day" date='30day'><h1 class="btn19">1개월</h1></a>
-		<a class="bbtn set_day" date='60day'><h1 class="btn19">3개월</h1></a>
-		<a class="bbtn set_day" date='120day'><h1 class="btn19">6개월</h1></a>
+		<a class="bbtn set_day" date='today'><h1 class="btn19">Өнөөдөр</h1></a>
+		<a class="bbtn set_day" date='week'><h1 class="btn19">Энэ 7 хоног</h1></a>
+		<a class="bbtn set_day" date='month'><h1 class="btn19">Энэ сар</h1></a>
+		<a class="bbtn set_day" date='7day'><h1 class="btn19">1 долоо хоног</h1></a>
+		<a class="bbtn set_day" date='15day'><h1 class="btn19">15 өдөр</h1></a>
+		<a class="bbtn set_day" date='30day'><h1 class="btn19">1 сар</h1></a>
+		<a class="bbtn set_day" date='60day'><h1 class="btn19">3 сар</h1></a>
+		<a class="bbtn set_day" date='120day'><h1 class="btn19">6 сар</h1></a>
 		&nbsp;
-		<span class="cbtn grf_col lnb_col" style="width:40px"><input type='submit' class="btn19 b" onFocus="blur()" value='검색'/></span>
-		<span class="btn"><input type='button' class="btn19 b" onFocus="blur()" onclick="$('#statisticsSearchFrm').resetForm();" value='초기화'></span>
+		<span class="cbtn grf_col lnb_col" style="width:40px"><input type='submit' class="btn19 b" onFocus="blur()" value='Хайх'/></span>
+		<span class="btn"><input type='button' class="btn19 b" onFocus="blur()" onclick="$('#statisticsSearchFrm').resetForm();" value='Эхлэл'></span>
 	</dl>
 
 	</form>
