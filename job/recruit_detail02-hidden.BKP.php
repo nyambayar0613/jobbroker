f2c22bdc3a2afa21123c6e2368b7db38e6a5c127
maralgua <bmaral0926@gmail.com>
2021-06-14 14:58:06 -0700

    орчуулга хийгдсэн.

--- job/recruit_detail02-hidden.BKP.php
+++ job/recruit_detail02-hidden.BKP.php
@@ -19,9 +19,9 @@ include_once(NFE_PATH.'/head.php');
 
 			<div class="lnb">
 			  <div class="navi_menu">
-                  <a href="recruit_main.php" class="active">Ажлын байрны мэдээлэл</a>
-                  <a href="person_main.php">Хүний нөөц</a>
-                  <a href="community_main.php">Комиунити</a>
+					<a href="recruit_main.php" class="active">구인정보</a>
+					<a href="person_main.php">인재정보</a>
+					<a href="community_main.php">커뮤니티</a>
 				</div>
 			</div>
 		</header>
@@ -29,12 +29,12 @@ include_once(NFE_PATH.'/head.php');
 			<div class="container">
 
         <section class="cont_box detail_con">
-				  <h2 class="top_tit">Онлайн худалдааны төвийн цагийн ажилд зуучлах (агуулахын менежмент, бүтээгдэхүүн хүргэх, савлах)</h2>
+				  <h2 class="top_tit">온라인 쇼핑몰 아르바이트 모집(창고 물류 관리, 상품 택배 포장)</h2>
           <div class="top_area">
             <ul>
-							<li class="etc">Эцсийн хугацаа</li>
-                <li class="ktid"><span>Kakao talk ID : <em>kakao-id</em></span></li>
-                <li class="btn_report"><a href="#"><img src="/images/icon_notify.gif" alt="Мэдэгдэх">Мэдэгдэх</a></li>
+							<li class="etc">마감일 상시모집</li>		
+							<li class="ktid"><span>카톡ID : <em>kakao-id</em></span></li>		
+							<li class="btn_report"><a href="#"><img src="/images/icon_notify.gif" alt="신고하기">신고</a></li>
 						</ul>
 					</div>
 				  <div class="company_con cf">
@@ -42,16 +42,16 @@ include_once(NFE_PATH.'/head.php');
 							<img src="/images/logo01.png" alt="LOGO">
 						</div>
 						<div class="text_box">
-							<div class="company">Finance wu LLC</div>
+							<div class="company">(주)파이낸뷰</div>
 						</div>
 					</div>
 					<div class="company_info cf">
 						<div class="mb_info ceo_info">
 							<div class="ceo_inner">
 								<dl>
-									<dt class="hd hd2"><span><img src="/images/info1-1.png" alt="Хариуцсан хүн"></span>Хариуцсан хүн</dt>
+									<dt class="hd hd2"><span><img src="/images/info1-1.png" alt="담당자"></span>담당자</dt>
 									<dd class="col1 col2">
-										<a href="#" class="hide">Ажлын байрны мэдээллийг үзэх үйлчилгээний өргөдөл</a>
+										<a href="#" class="hide">구인정보 열람서비스 신청</a>
 									</dd>
 								</ul>
 							</div>
@@ -59,36 +59,36 @@ include_once(NFE_PATH.'/head.php');
 						<div class="mb_info address_info">
 							<div class="address_inner">
 								<dl>
-									<dt class="hd hd2"><span><img src="/images/info1-2.png" alt="Холбогдох дугаар"></span>Холбогдох дугаар</dt>
+									<dt class="hd hd2"><span><img src="/images/info1-2.png" alt="연락처"></span>연락처</dt>
 									<dd class="col1 col2">
-										<a href="#" class="hide">Ажлын байрны мэдээллийг үзэх үйлчилгээний өргөдөл</a>
+										<a href="#" class="hide">구인정보 열람서비스 신청</a>
 									</dd>
 								</ul>
 							</div>
 						</div> 
 					</div>
 					<div class="button_group scrap_bt">
-                        <button type="button" class="bt-online">Ажилд орох өргөдөл (Онлайн)</button>
-                        <button type="button" class="bt-email">Ажилд орох өргөдөл (Мэйл)</button>
-                        <button type="button" class="bt-scrap"><img src="/images/scrap_icon2.png" alt="scrab"><!--<img src="/images/scrap_icon1.png">-->Scrab</button>
+					  <button type="button" class="bt-online">온라인 입사지원</button>
+						<button type="button" class="bt-email">이메일 입사지원</button>
+					  <button type="button" class="bt-scrap"><img src="/images/scrap_icon2.png" alt="스크랩"><!--<img src="/images/scrap_icon1.png">-->스크랩</button>
 					</div>
 				</section>
 
 				<section class="cont_box detail_con detail_con2">
 				  <div class="tab1-con cf">
 					  <ul>
-                          <li><a href="#">Ажилд урьж байна</a></li>
-                          <li class="active"><a href="#">Дэлгэрэнгүй мэдээлэ</a></li>
-                          <li><a href="#">Ажлын байршил</a></li>
-                          <li><a href="#">Байгуууллагын тухай</a></li>
+						  <li><a href="#">모집요강</a></li>
+						  <li class="active"><a href="#">상세요강</a></li>
+						  <li><a href="#">근무위치</a></li>
+						  <li><a href="#">회사정보</a></li>
             </ul>
 					</div>
 					<div class="tab-box tab4-box">
 					  <div class="container cf">
               <div class="recruit_service">
-							  <p>Хэрэв та дэлгэрэнгүйг уншихыг хүсвэл ажлын байрны мэдээллийг үзэх үйлчилгээнд хамрагдана уу.</p>
+							  <p>상세요강 내용을 열람하시려면 구인정보 열람서비스를 신청하세요.</p>
 								<div class="member_btn">
-									<div class="recruit_service_bt"><a href="#">Ажлын байрны мэдээллийг үзэх үйлчилгээний хүсэлт</a></div>
+									<div class="recruit_service_bt"><a href="#">구인정보 열람서비스 신청</a></div>
 								</div>
 							</div>
 						</div>
@@ -133,7 +133,7 @@ include_once(NFE_PATH.'/head.php');
 					<ul class="cont_box_inner">
 						<li>
 							<div class="text_box">
-								<div class="title"><a href="#"> Худалдааны төв цагийн ажил ажилд зуучлах онлайн дэлгүүр худалдааны төв цагийн ажил ажилд зуучлах</a><span class="n_date">2019.10.16</span></div>
+								<div class="title"><a href="#">온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집</a><span class="n_date">2019.10.16</span></div>
 							</div>
 						</li>
 					</ul>
