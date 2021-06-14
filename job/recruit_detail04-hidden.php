7502b835c0e86476c449c0ad9fe85a826a8aa4b9
maralgua <bmaral0926@gmail.com>
2021-06-14 16:28:38 -0700

    орчуулга хийгдсэн.

--- job/recruit_detail04-hidden.php
+++ job/recruit_detail04-hidden.php
@@ -2,7 +2,7 @@
 include_once('./_common.php');
 
 define('_INDEX_', true);
-if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가гү
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가лшгү
 
 if(defined('G5_THEME_PATH')) {
     require_once(G5_THEME_PATH.'/index.php');
