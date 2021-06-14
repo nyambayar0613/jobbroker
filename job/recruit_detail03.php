9436f76fc702b4858a306bb5bdc3350f6004c08a
maralgua <bmaral0926@gmail.com>
2021-06-14 15:09:24 -0700

    орчуулга хийгдсэн.

--- job/recruit_detail03.php
+++ job/recruit_detail03.php
@@ -19,9 +19,9 @@ include_once(NFE_PATH.'/head.php');
 
 			<div class="lnb">
 			  <div class="navi_menu">
-					<a href="recruit_main.php" class="active">구인정보</a>
-					<a href="person_main.php">인재정보</a>
-					<a href="community_main.php">커뮤니티</a>
+                  <a href="recruit_main.php" class="active">Ажлын байрны мэдээлэл</a>
+                  <a href="person_main.php">Хүний нөөц</a>
+                  <a href="community_main.php">Комиунити</a>
 				</div>
 			</div>
 		</header>
@@ -31,60 +31,60 @@ include_once(NFE_PATH.'/head.php');
 	      <section class="cont_box detail_con">
           <div class="top_area">
             <ul>
-							<li class="ktid"><span>카톡ID : <em>kakao-id</em></span></li>		
-							<li class="btn_report"><a href="#"><img src="/images/icon_notify.gif" alt="신고하기">신고</a></li>
+                <li class="ktid"><span>Kakao talk ID : <em>kakao-id</em></span></li>
+                <li class="btn_report"><a href="#"><img src="/images/icon_notify.gif" alt="Мэдэгдэх">Мэдэгдэх</a></li>
 						</ul>
 					</div>
 					<div class="logo_box">
 						<img src="/images/logo01.png" alt="LOGO">
 					</div>
 					<div class="text_box">
-					  <div class="company">(주)파이낸뷰</div>
-						<div class="title">온라인 쇼핑몰 아르바이트 모집(창고 물류 관리, 상품 택배 포장)</div>
-						<div class="info">마감일 상시모집</div>
+                        <div class="company">Finance wu LLC</div>
+                        <div class="title">Онлайн худалдааны төвийн цагийн ажилд зуучлах (агуулахын менежмент, бүтээгдэхүүн хүргэх, савлах)</div>
+                        <div class="info">Ажилд авах эцсийн хугацаа</div>
 					</div>
 					<div class="adm_name">
 					  <div class="name_inner">
 						  <dl>
-							  <dt class="hd"><span><img src="/images/info1.png" alt="담당자"></span>담당자</dt>
-								<dd class="col1">김범</dd>
+                              <dt class="hd"><span><img src="/images/info1.png" alt="Хариуцсан хүн"></span>Хариуцсан  хүн</dt>
+                              <dd class="col1">Mr Kim</dd>
 							</ul>
 						</div>
 					</div>
 					<div class="tel_info">
 					  <div class="tel_inner">
 						  <dl>
-							  <dt class="hd"><span><img src="/images/info2.png" alt="연락처"></span>연락처</dt>
-								<dd class="col1">010-1234-5678</dd>
-								<dd class="col2"><a href="#"><span><img src="/images/tel_ico.png" alt="전화하기"></span><em>전화하기</em></a></dd>
-							</dl>
-						</div>
-					</div>
-					<div class="button_group scrap_bt">
-					  <button type="button" class="bt-online">온라인 입사지원</button>
-						<button type="button" class="bt-email">이메일 입사지원</button>
-					  <button type="button" class="bt-scrap"><img src="/images/scrap_icon2.png" alt="스크랩"><!--<img src="/images/scrap_icon1.png">-->스크랩</button>
-					</div>	
-				</section>
+                              dt class="hd"><span><img src="/images/info2.png" alt="Холбогдох дугаар"></span>Холбогдох дугаар</dt>
+                              <dd class="col1">010-1234-5678</dd>
+                              <dd class="col2"><a href="#"><span><img src="/images/tel_ico.png" alt="Холбогдох"></span><em>Холбогдох дугаар</em></a></dd>
+                          </dl>
+                      </div>
+                    </div>
+              <div class="button_group scrap_bt">
+                  <button type="button" class="bt-online">Ажилд орох өргөдөл (Онлайн)</button>
+                  <button type="button" class="bt-email">Ажилд орох өргөдөл (Мэйл)</button>
+                  <button type="button" class="bt-scrap"><img src="/images/scrap_icon2.png" alt="scrab"><!--<img src="/images/scrap_icon1.png">-->Scrab</button>
+              </div>
+          </section>
 
-				<section class="cont_box detail_con detail_con2">
-				  <div class="tab1-con cf">
-					  <ul>
-						  <li><a href="#">모집요강</a></li>
-						  <li><a href="#">상세요강</a></li>
-						  <li class="active"><a href="#">근무위치</a></li>
-						  <li><a href="#">회사정보</a></li>
+                <section class="cont_box detail_con detail_con2">
+                    <div class="tab1-con cf">
+                        <ul>
+                            <li><a href="#">Ажилд урьж байна</a></li>
+                            <li class="active"><a href="#">Дэлгэрэнгүй мэдээлэ</a></li>
+                            <li><a href="#">Ажлын байршил</a></li>
+                            <li><a href="#">Байгуууллагын тухай</a></li>
             </ul>
 					</div>
 					<div class="tab-box tab3-box">
             <table>
 						  <tr>
-							  <th>근무지주소</th>
-								<td>광주광역시 동구 금남로5가 152번지 남선빌딩 609호</td>
+							  <th>Ажлын байршил</th>
+								<td>Namwon Building, 609 тоот, Гванжу, Dong-gu, 152 Geumnam-ro 5-ga.</td>
 							</tr>
 						  <tr>
-							  <th>인근지하철</th>
-								<td><span class="subway">6호선</span> 디지털미디어시티</td>
+							  <th>Ойролцоох метро</th>
+								<td><span class="subway">6 гарц</span> digital media city</td>
 							</tr>
             </table>
 					</div>
@@ -131,7 +131,7 @@ include_once(NFE_PATH.'/head.php');
 					<ul class="cont_box_inner">
 						<li>
 							<div class="text_box">
-								<div class="title"><a href="#">온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집</a><span class="n_date">2019.10.16</span></div>
+								<div class="title"><a href="#">Худалдааны төв цагийн ажил ажилд зуучлах онлайн дэлгүүр худалдааны төв цагийн ажил ажилд зуучлах</a><span class="n_date">2019.10.16</span></div>
 							</div>
 						</li>
 					</ul>
