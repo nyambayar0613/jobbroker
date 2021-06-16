543988b60e0795114c8ed2d271788192d79b3908
maralgua <bmaral0926@gmail.com>
2021-06-16 10:37:33 -0700

    орчуулга хийгдсэн.

--- include/inc/adver/service1.inc.php
+++ include/inc/adver/service1.inc.php
@@ -40,7 +40,7 @@ if($total['c']>0) {
 </style>
 <?php }?>
 <section class="service1__ cont_box platinum_con">
-	<h2><span class="tit_ico"><img src="/images/<?=$netfu_util->icon_tit('title_icon01');?>.png" alt=""></span>Пилатинум ажлын зар<span class="bt_box"><?=$_count;?> <a href="<?=NFE_URL;?>/etc/adver.php"><span class="btn">광고안내<img src="<?=NFE_URL;?>/images/chevron.png" alt="광고안내"></span></a></span></h2>
+	<h2><span class="tit_ico"><img src="/images/<?=$netfu_util->icon_tit('title_icon01');?>.png" alt=""></span> Пилатинум ажлын зар<span class="bt_box"><?=$_count;?> <a href="<?=NFE_URL;?>/etc/adver.php"><span class="btn">광고안내<img src="<?=NFE_URL;?>/images/chevron.png" alt="광고안내"></span></a></span></h2>
 	<div class="cycle-slideshow" 
 	data-cycle-pause-on-hover="true"
 	data-cycle-slides="ul.platinum_box"
