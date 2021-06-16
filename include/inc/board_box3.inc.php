435004be2f3e2e38bc40b152c0862fdb8210a696
maralgua <bmaral0926@gmail.com>
2021-06-16 10:43:07 -0700

    орчуулга хийгдсэн.

--- include/inc/board_box3.inc.php
+++ include/inc/board_box3.inc.php
@@ -2,9 +2,9 @@
 	<a href="#"><img src="/images/sample_img02.png" alt=""></a>
 </div>
 <div class="text_box">
-	<div class="title"><a href="#">온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집</a></div>
+    <div class="title"><a href="#">Онлайн худалдааны төвийн цагийн ажилд зуучлах (агуулахын менежмент, бүтээгдэхүүн хүргэлт, сав баглаа боодол)</a></div>
 	<div class="info">
-	<span class="name">관리자</span>
+        <span class="name">Админ</span>
 		<span class="date">2019.10.16</span>
 	</div>
 </div>
