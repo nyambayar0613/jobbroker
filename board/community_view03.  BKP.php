9b71d63cf27b2af4e9a4f3a0cc184b9568619072
maralgua <bmaral0926@gmail.com>
2021-06-15 17:42:02 -0700

    орчуулга хийгдсэн.

--- board/community_view03.  BKP.php
+++ board/community_view03.  BKP.php
@@ -19,9 +19,9 @@ include_once(NFE_PATH.'/head.php');
 
 			<div class="lnb">
 			  <div class="navi_menu">
-                  <a href="recruit_main.php" class="active">Ажлын байрны мэдээлэл</a>
-                  <a href="person_main.php">Хүний нөөц</a>
-                  <a href="community_main.php">Комиунити</a>
+					<a href="recruit_main.php">구인정보</a>
+					<a href="person_main.php">인재정보</a>
+					<a href="community_main.php" class="active">커뮤니티</a>
 				</div>
 			</div>
 		</header>
@@ -34,17 +34,17 @@ include_once(NFE_PATH.'/head.php');
           <div class="community_inner cf">
 						<div class="view_wrap">
 						  <div class="view_top cf">
-                              <div class="view_title"><a href="#">Coffeeshop-т цагийн ажил хийх тухай ~ </a></div>
-                              <div class="view_info">
-                                  <span class="mb_id"><strong>Netfu цагын ажил</strong></span>
-                                  <span>2019.11.08&nbsp;&nbsp;12:11</span>
-                                  <span class="hits">Үзсэн тоо : <em>14</em></span>
+								<div class="view_title"><a href="#">커피전문점 알바에 관해서~ 커피전문점 알바에 관해서~ 커피전문점 알바에 관해서~ 커피전문점 알바에 관해서~ 커피전문점 알바에 관해서~ 커피전문점 알바에 관해서~ 커피전문점 알바에 관해서~ 커피전문점 알바에 관해서~ 커피전문점 알바에 관해서~ </a></div>
+								<div class="view_info">
+									<span class="mb_id"><strong>넷퓨알바</strong></span>
+									<span>2019.11.08&nbsp;&nbsp;12:11</span>
+									<span class="hits">조회수 : <em>14</em></span>
 								</div>
 							</div>
 							<div class="view_con cf">
                 <img src="/images/photo2.jpg" alt="">
 							  <p>
-[Q] Энэ ажлыг яаж олсон бэ?<br>
+[Q] 어떻게 그 아르바이트를 알게 되었나요?<br>
 방학을 알차게 보내기 위해 아르바이트를 검색하던 중 알게 되었습니다.<br>
 사실 방학때 아무런 계획이 없었기에 뭐 할꺼 없나 이것저것 검색하던중에 알게 되었습니다.<br>
 커피전문점이라서 로스팅에서 부터 다양한 라떼아트까지 배울께 너무 많아서 좋네요~~~ 화이팅<br>								
@@ -55,10 +55,10 @@ include_once(NFE_PATH.'/head.php');
 
 					<div class="button-group view_bt">
 						<ul>
-                            li><a href="#">Жагсаалт</a></li>
-                            <li><a href="#">Өөрчлөх</a></li>
-                            <li><a href="#">Устгах</a></li>
-                            <li><a href="#">Бичсэн</a></li>
+							<li><a href="#">목록</a></li>
+							<li><a href="#">수정</a></li>
+							<li><a href="#">삭제</a></li>
+							<li><a href="#">글작성</a></li>
 						</ul>
 					</div>
 				</section>
@@ -66,19 +66,19 @@ include_once(NFE_PATH.'/head.php');
 
 				<!-- 댓글 -->
         <section class="cont_box comment_con">
-            <h2>Сэтгэгдэл<span>999</span></h2>
-            <div class="comment_box cf">
-                <div class="cmt_write">
-                    <div class="cmt_hd cf">
-                        <ul>
-                            <li class="wr_name"><label>Нэр<input type="text" name="" id=""></label></li>
-                            <li class="wr_pw"><label>Нууц дугаар<input type="password" name="" id="" maxlength="16"></label></li>
-                            <li class="captcha_key"><label>Автомат бүртгэлээс сэргийлэх<input type="text" name="" id=""></label><span>777777</span></li>
-                        </ul>
-                    </div>
-                    <div class="input_box">
-                        <div class="text-box"><textarea rows="3" placeholder="Сэтгэгдэл бичнэ үү."></textarea></div>
-                        <button>Бүртгэх</button>
+				  <h2>댓글<span>999</span></h2>
+				  <div class="comment_box cf">
+					  <div class="cmt_write">
+						  <div class="cmt_hd cf">
+							  <ul>
+								  <li class="wr_name"><label>이름<input type="text" name="" id=""></label></li>
+									<li class="wr_pw"><label>비밀번호<input type="password" name="" id="" maxlength="16"></label></li>
+									<li class="captcha_key"><label>자동등록방지문자<input type="text" name="" id=""></label><span>777777</span></li>
+								</ul>
+							</div>
+							<div class="input_box">
+							  <div class="text-box"><textarea rows="3" placeholder="댓글을 입력하세요."></textarea></div>
+								<button>등록</button>
 							</div>
 						</div>
 					</div>
