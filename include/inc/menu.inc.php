d9faaba1e6e8d1334885bb669dfb72953f6e7c4e
maralgua <bmaral0926@gmail.com>
2021-06-16 13:06:31 -0700

    орчуулга хийгдсэн.

--- include/inc/menu.inc.php
+++ include/inc/menu.inc.php
@@ -5,7 +5,7 @@ if(strpos($_SERVER['PHP_SELF'], '/resume/')!==false) $menu_on = 'resume';
 if(strpos($_SERVER['PHP_SELF'], '/board/')!==false) $menu_on = 'board';
 ?>
 <div class="navi_menu">
-	<a href="<?=NFE_URL;?>/job/index.php" class="<?=$menu_on=='job' ? 'active' : '';?>">구인정보</a>
-	<a href="<?=NFE_URL;?>/resume/index.php" class="<?=$menu_on=='resume' ? 'active' : '';?>">인재정보</a>
-	<a href="<?=NFE_URL;?>/board/index.php" class="<?=$menu_on=='board' ? 'active' : '';?>">커뮤니티</a>
+	<a href="<?=NFE_URL;?>/job/index.php" class="<?=$menu_on=='job' ? 'active' : '';?>">Ажлын байрны мэдээлэл</a>
+	<a href="<?=NFE_URL;?>/resume/index.php" class="<?=$menu_on=='resume' ? 'active' : '';?>">Хүний нөөц</a>
+	<a href="<?=NFE_URL;?>/board/index.php" class="<?=$menu_on=='board' ? 'active' : '';?>">Комиунити</a>
 </div>
