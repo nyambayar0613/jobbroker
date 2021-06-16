7554d07b687cab46500091364348dc1e31a9f0f2
maralgua <bmaral0926@gmail.com>
2021-06-16 10:59:38 -0700

    орчуулга хийгдсэн.

--- include/inc/job_detail_tab2.inc.php
+++ include/inc/job_detail_tab2.inc.php
@@ -8,9 +8,9 @@ if(!$info['is_open__']){
 ?>
 <div class="container bnr_bx cf">
 	<div class="recruit_service cf">
-		<p>상세요강 내용을 열람하시려면 구인정보 열람서비스를 신청하세요.</p>
+		<p>Та дэлгэрэнгүй уншихыг хүсвэл ажлын байрны мэдээллийг үзэх үйлчилгээнд хамрагдана уу.</p>
 		<div class="member_btn">
-			<div class="recruit_service_bt"><a href="<?=NFE_URL;?>/payment/read_payment.php">구인정보 열람서비스 신청</a></div>
+			<div class="recruit_service_bt"><a href="<?=NFE_URL;?>/payment/read_payment.php">Ажлын байрны мэдээллийг үзэх үйлчилгээний өргөдөл</a></div>
 		</div>
 	</div>
 </div>
