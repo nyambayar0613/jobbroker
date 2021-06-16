47ec7486d96f9b5ee27565fd00d0cbe981ee7079
maralgua <bmaral0926@gmail.com>
2021-06-16 10:48:16 -0700

    орчуулга хийгдсэн.

--- include/inc/job_box1.inc.php
+++ include/inc/job_box1.inc.php
@@ -22,7 +22,7 @@ if($row) {
 	$_gold = $row['wr_service_platinum_main_gold']>=date("Y-m-d") ? 'gold2' : 'gold1'; // : 골드 클래스값
 	$_logo_c = $netfu_mjob->get_logo_type('wr_service_platinum_main_logo', $row, $get_logo);
 ?>
-<button id="show_ly" class="more_option show_ly job_more_option" no="<?=$row['no'];?>"><span>더보기</span></button>
+<button id="show_ly" class="more_option show_ly job_more_option" no="<?=$row['no'];?>"><span>Цааш үзэх</span></button>
 <div class="text_box bg_ gold_bx">
 	<a href="<?=NFE_URL;?>/job/detail.php?no=<?=$row['no'];?>"><div class="logo_box <?=$_logo_c[2];?>" >
 		<?=$_logo_c['img'];?>
@@ -40,8 +40,8 @@ if($row) {
 <?php } else {?>
 <div class="white_box platinum-wbx">
 	<div class="whitebox_inner">
-		<div class="text">신규광고 등록 대기중</div>
-		<a href="<?=NFE_URL;?>/etc/adver.php"><div class="btn">광고안내 및 신청<img src="/images/chevron.png" alt="광고안내 및 신청"></div></a>
+		<div class="text">Зар сурталчилгааны бүртгэл хүлээгдэж байна.</div>
+		<a href="<?=NFE_URL;?>/etc/adver.php"><div class="btn">Зар сурталчилгааны мэдээлэл болон хүсэлт<img src="/images/chevron.png" alt="Зар сурталчилгааны мэдээлэл болон хүсэлт"></div></a>
 	</div>
 </div>
 <?php }?>
