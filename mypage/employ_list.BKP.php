0776da1af060eea1f1c241bf921cec2f72f0240d
maralgua <bmaral0926@gmail.com>
2021-06-17 13:54:58 -0700

    орчуулга хийгдсэн.

--- mypage/employ_list.BKP.php
+++ mypage/employ_list.BKP.php
@@ -48,15 +48,15 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 
 	<div class="resume_list_con cf">
 		<ul class="list-tab">
-			<li class="tab01 <?=$_GET['code']!='end' ? 'active' : '';?>"><a href="<?=$_SERVER['PHP_SELF'];?>?code=ing">Үргэлжилж буй зар<span class="list_num"><?=number_format($_my_count['job_ing']['c']);?></span></a></li>
-			<li class="tab02 <?=$_GET['code']=='end' ? 'active' : '';?>"><a href="<?=$_SERVER['PHP_SELF'];?>?code=end">Хугацаа дууссан ажлын зар<span class="list_num"><?=number_format($_my_count['job_end']['c']);?></span></a></li>
+			<li class="tab01 <?=$_GET['code']!='end' ? 'active' : '';?>"><a href="<?=$_SERVER['PHP_SELF'];?>?code=ing">진행중인 구인공고<span class="list_num"><?=number_format($_my_count['job_ing']['c']);?></span></a></li>
+			<li class="tab02 <?=$_GET['code']=='end' ? 'active' : '';?>"><a href="<?=$_SERVER['PHP_SELF'];?>?code=end">마감된 구인공고<span class="list_num"><?=number_format($_my_count['job_end']['c']);?></span></a></li>
 		</ul>
 		<form name="fsearch2" action="<?=$_SERVER['PHP_SELF'];?>" method="get">
 		<ul class="list_con search_gp">
-			<li class="sch_bt"><input type="radio" name="search_field" checked value="wr_company_name"><label for="company">Байгууллага</label></li>
-			<li class="sch_bt"><input type="radio" name="search_field" value="wr_subject" <?=$_GET['search_field']=='wr_subject' ? 'checked' : '';?>><label for="title">Зарын гарчиг</label></li>
-			<li class="sch_bt"><input type="radio" name="search_field" value="wr_person" <?=$_GET['search_field']=='wr_person' ? 'checked' : '';?>><label for="manager">Хариуцагчын нэр</label></li>
-			<li class="sch_bar"><input type="text" name="search_keyword" value="<?=$_GET['search_keyword'];?>"><button class="plus_bt plus_bt3" onClick="document.forms['fsearch2'].submit()">Хайх</button></li>
+			<li class="sch_bt"><input type="radio" name="search_field" checked value="wr_company_name"><label for="company">근무회사</label></li>
+			<li class="sch_bt"><input type="radio" name="search_field" value="wr_subject" <?=$_GET['search_field']=='wr_subject' ? 'checked' : '';?>><label for="title">구인공고 제목</label></li>
+			<li class="sch_bt"><input type="radio" name="search_field" value="wr_person" <?=$_GET['search_field']=='wr_person' ? 'checked' : '';?>><label for="manager">담당자명</label></li>
+			<li class="sch_bar"><input type="text" name="search_keyword" value="<?=$_GET['search_keyword'];?>"><button class="plus_bt plus_bt3" onClick="document.forms['fsearch2'].submit()">검색</button></li>
 		</ul>
 		</form>
 		
@@ -69,7 +69,7 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 		?>
 		<ul class="list_con">
 			<li class="col2 none">
-				<div class="list_txt2"><img src="<?=NFE_URL;?>/images/info.png" alt="">Бүртгэлтэй мэдээлэл байхгүй байна.</div>
+				<div class="list_txt2"><img src="<?=NFE_URL;?>/images/info.png" alt="">등록된 공고가 없습니다.</div>
 			</li>
 		</ul>
 		<?php
@@ -101,8 +101,8 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 			<li class="col1"><input type="checkbox" id="" name="chk[]" value="<?=$row['no'];?>"></li>
 			<li class="col2"><a href="<?=NFE_URL;?>/job/detail.php?no=<?=$row['no'];?>">
 				<div class="list_txt"><?=strip_tags(stripslashes($row['wr_subject']));?></div>
-				<div class="list_etc"><span class="co_name"><?=strip_tags(stripslashes($row['wr_company_name']));?></span><span>Хүсэлт гаргагч : <i><?=number_format($val['wr_desire']);?></i>명</span></div>
-				<div class="list_etc"><span class="date">Бичсэн : <?=$netfu_util->get_date('dot', $row['wr_wdate']);?></span><span><?=$info['volume_text'];?></span></div>
+				<div class="list_etc"><span class="co_name"><?=strip_tags(stripslashes($row['wr_company_name']));?></span><span>지원자 : <i><?=number_format($val['wr_desire']);?></i>명</span></div>
+				<div class="list_etc"><span class="date">작성일 : <?=$netfu_util->get_date('dot', $row['wr_wdate']);?></span><span><?=$info['volume_text'];?></span></div>
 				<?php
 				foreach($job_type_arr2 as $k=>$v) {
 					if(!$v[0]) continue;
@@ -111,23 +111,23 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 				<?php }?>
 			</a></li>
 			<li class="col3">
-				<div class="list_btn list_btn1 list_btn1-1"><a href="./job_write.php?no=<?=$row['no'];?>" class="btn_a" style="color:#3694ed">Өөрчлөх</a></div>
-				<div class="list_btn list_btn1 list_btn1-2"><a href="./job_write.php?mode=load&no=<?=$row['no'];?>" class="btn_a" style="color:#3694ed">Хувилах</a></div>
-				<div class="list_btn list_btn1 list_btn1-1"><button type="button" onClick="netfu_util1.delete_func('<?=$row['no'];?>', 'job')">Устгах</button></div>
+				<div class="list_btn list_btn1 list_btn1-1"><a href="./job_write.php?no=<?=$row['no'];?>" class="btn_a" style="color:#3694ed">수정</a></div>
+				<div class="list_btn list_btn1 list_btn1-2"><a href="./job_write.php?mode=load&no=<?=$row['no'];?>" class="btn_a" style="color:#3694ed">복사</a></div>
+				<div class="list_btn list_btn1 list_btn1-1"><button type="button" onClick="netfu_util1.delete_func('<?=$row['no'];?>', 'job')">삭제</button></div>
 				<?php if($_GET['code']!='end') {?><div class="list_btn list_btn1 list_btn1-2"><a href="#none;" onClick="netfu_mjob.alba_finish('<?=$row['no'];?>')"><button type="button">마감</button></a></div><?php }?>
-				<div class="list_btn list_btn1 list_btn1-1"><a href="./job_write.php" class="btn_a" style="color:#3694ed">Зар бүртгүүлэх</a></div>
+				<div class="list_btn list_btn1 list_btn1-1"><a href="./job_write.php" class="btn_a" style="color:#3694ed">공고등록</a></div>
 				<?php if($_GET['code']!='end') {?>
-				<div class="list_btn list_btn1 list_btn1-2" onClick="netfu_mjob.jump_func('alba', '<?=$row['no'];?>')" class="btn_a"><button type="button">Jump</button></div>
+				<div class="list_btn list_btn1 list_btn1-2" onClick="netfu_mjob.jump_func('alba', '<?=$row['no'];?>')" class="btn_a"><button type="button">점프</button></div>
 				<?php
 				}
 				$_field = 'mb_alba_jump_count';
 				if($member_service[$_field]>0) {
 				?>
-				<div class="list_btn list_btn1-3"><button type="button">Jump ашиглаж байна</button></div>
+				<div class="list_btn list_btn1-3"><button type="button">점프사용중</button></div>
 				<?php } else {?>
-				<div class="list_btn list_btn1-4"><button type="button">Jump ашиглаагүй байна</button></div>
+				<div class="list_btn list_btn1-4"><button type="button">점프미사용</button></div>
 				<?php }?>
-				<a href="<?=NFE_URL;?>/payment/job_payment.php?no=<?=$row['no'];?>"><div class="list_btn list_btn2" class="btn_a" style="color:#fff">Үйлчилгээний хүсэлт</div></a>
+				<a href="<?=NFE_URL;?>/payment/job_payment.php?no=<?=$row['no'];?>"><div class="list_btn list_btn2" class="btn_a" style="color:#fff">서비스신청</div></a>
 			</li>
 
 			<?php
@@ -135,12 +135,12 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 			?>
 			<li class="col4 cf" style="clear:both">
 				<ul>
-					<h3>Төлбөртэй үйлчилгээний ашиглалтын түүх</h3>
+					<h3>유료서비스 이용내역</h3>
 					<?php
 					if(is_array($_remain_service)) { foreach($_remain_service as $k=>$v) {
 					?>
 					<li>
-						<p><?=$v;?></p><a href="<?=NFE_URL;?>/payment/job_payment.php?no=<?=$row['no'];?>">Үйлчилгээ сунгах</a>
+						<p><?=$v;?></p><a href="<?=NFE_URL;?>/payment/job_payment.php?no=<?=$row['no'];?>">서비스연장</a>
 					</li>
 					<?php
 					} }
@@ -161,7 +161,7 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 </section>
 
 <div class="button_con">
-	<a href="/mypage/job_write.php" class="bottom_btn01" onClick="">Ажлын байрны мэдэээлэл бүртгэх</a><a href="#none;" onClick="netfu_util1.delete_select_func(document.forms['flist'])" class="bottom_btn02">Устгах</a>
+	<a href="/mypage/job_write.php" class="bottom_btn01" onClick="">구인정보등록</a><a href="#none;" onClick="netfu_util1.delete_select_func(document.forms['flist'])" class="bottom_btn02">삭제</a>
 </div>
 
 <!--
