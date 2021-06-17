6aed4c93afe4e4b062c7876afd0c3ec552025d3c
maralgua <bmaral0926@gmail.com>
2021-06-17 14:13:22 -0700

    орчуулга хийгдсэн.

--- mypage/job_list.php
+++ mypage/job_list.php
@@ -18,32 +18,32 @@ include NFE_PATH.'/include/inc/my_company_count.inc.php';
 		<li class="tab02"><a href="#">Хугацаа дууссан ажлын зар<span class="list_num">1</span></a></li>
 	</ul>
 	<ul class="list_con search_gp">
-		<li class="sch_bt"><input type="radio" id="" name="search_slt" checked="checked"><label for="company">근무회사</label></li>
-		<li class="sch_bt"><input type="radio" id="" name="search_slt"><label for="title">구인공고 제목</label></li>
-		<li class="sch_bt"><input type="radio" id="" name="search_slt"><label for="manager">담당자명</label></li>
-		<li class="sch_bar"><input type="text" id="" name=""><button class="plus_bt plus_bt3">검색</button></li>
+		<li class="sch_bt"><input type="radio" id="" name="search_slt" checked="checked"><label for="company">Байгууллага</label></li>
+		<li class="sch_bt"><input type="radio" id="" name="search_slt"><label for="title">Зарын гарчиг</label></li>
+		<li class="sch_bt"><input type="radio" id="" name="search_slt"><label for="manager">Хариуцсан хүний нэр</label></li>
+		<li class="sch_bar"><input type="text" id="" name=""><button class="plus_bt plus_bt3">Хайх</button></li>
 	</ul>
 	<ul class="list_con">
 		<li class="col2 none">
-			<div class="list_txt2"><img src="images/info.png" alt="">등록된 공고가 없습니다.</div>
+			<div class="list_txt2"><img src="images/info.png" alt="">Бүртгэлтэй мэдээлэл байхгүй байна.</div>
 		</li>
 	</ul>
 	<ul class="list_con">
 		<li class="col1"><input type="checkbox" id="" name=""></li>
 		<li class="col2">
-			<div class="list_txt">온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집 온라인 쇼핑몰 아르바이트 모집</div>
-			<div class="list_etc"><span class="co_name">(주)파이낸뷰</span><span>지원자 : <i>0</i>명</span></div>
-			<div class="list_etc"><span class="date">작성일 : 2019.11.15</span><span>상시모집</span></div>
-			<div class="list_etc"><span>사무·회계, 사무보조, 문서정리</span></div>
+			<div class="list_txt">Онлайн худалдааны төвийн цагийн ажилд зуучлах Онлайн худалдааны төвийн цагийн ажил</div>
+			<div class="list_etc"><span class="co_name">Financial View Co., Ltd.</span><span>Өргөдөл гаргагч : <i>0</i>хүн</span></div>
+			<div class="list_etc"><span class="date">Бичсэн : 2019.11.15</span><span>Тогтмол элсэлт</span></div>
+			<div class="list_etc"><span>Оффис / нягтлан бодох бүртгэл, оффисын туслах, баримт бичгийн байгууллага</span></div>
 		</li>
 		<li class="col3">
-			<div class="list_btn list_btn1 list_btn1-1"><button type="button">수정</button></div>
-			<div class="list_btn list_btn1 list_btn1-2"><button type="button">복사</button></div>
-			<div class="list_btn list_btn1 list_btn1-1"><button type="button">삭제</button></div>
-			<div class="list_btn list_btn1 list_btn1-2"><button type="button">마감</button></div>
-			<div class="list_btn list_btn1 list_btn1-1"><button type="button">재등록</button></div>
-			<div class="list_btn list_btn1 list_btn1-2"><button type="button">점프</button></div>
-			<div class="list_btn list_btn2"><button type="button">서비스신청</button></div>
+			<div class="list_btn list_btn1 list_btn1-1"><button type="button">Өөрчлөх</button></div>
+			<div class="list_btn list_btn1 list_btn1-2"><button type="button">Хувилах</button></div>
+			<div class="list_btn list_btn1 list_btn1-1"><button type="button">Устгах</button></div>
+			<div class="list_btn list_btn1 list_btn1-2"><button type="button">Хугацаа дууссан</button></div>
+			<div class="list_btn list_btn1 list_btn1-1"><button type="button">Дахин бүртгэл</button></div>
+			<div class="list_btn list_btn1 list_btn1-2"><button type="button">Jump</button></div>
+			<div class="list_btn list_btn2"><button type="button">Үйлчилгээний програм</button></div>
 		</li>
 	</ul>
 	<ul class="list_con">
@@ -61,7 +61,7 @@ include NFE_PATH.'/include/inc/my_company_count.inc.php';
 </section>
 
 <div class="button_con button_con3">
-	<a href="#" class="bottom_btn03">삭제</a>
+	<a href="#" class="bottom_btn03">Устгах</a>
 </div>
 <?php
 include "../include/tail.php";
