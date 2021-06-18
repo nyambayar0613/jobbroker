aacfb6c3cc8dca65a5a5298481d38b4d932765ba
maralgua <bmaral0926@gmail.com>
2021-06-18 11:52:47 -0700

    орчуулга хийгдсэн.

--- mypage/member_leave.php
+++ mypage/member_leave.php
@@ -1,6 +1,6 @@
 <?php
 $page_code = 'mypage';
-$head_title = $menu_text = '회원탈퇴 신청';
+$head_title = $menu_text = 'Гишүүүнчлэлээс гарах хүсэлт';
 include "../include/top.php";
 
 //
@@ -10,7 +10,7 @@ $_cate_['member_left_reason'] = $netfu_util->get_cate_array('member_left_reason'
 var member_left = function() {
 	var form = document.forms['fwrite'];
 	if(!$(form).find("[name='left_agreement']:checked").val()) {
-		alert("회원탈퇴 유의사항에 동의해 주세요.");
+        alert("Гишүүнчлэл цуцлах тухай мэдэгдлийг зөвшөөрч байна уу.");
 	} else {
 		netfu_util1.ajax_submit(form);
 	}
@@ -19,12 +19,12 @@ var member_left = function() {
 <form name="fwrite" action="<?=NFE_URL;?>/regist.php" method="post">
 <input type="hidden" name="mode" value="member_left" />
 <section class="cont_box remove_con">
-	<h2>회원탈퇴 신청</h2>
+	<h2>Гишүүүнчлэлээс гарах хүсэлт</h2>
 	<div class="info4_con info_text">
 		<ol>
-			<li>1. 사용하고 계신 아이디 <strong><?=$member['mb_id'];?></strong>는 재사용 및 복구가 불가능합니다.</li>
-			<li>2. 추후 재가입 시 동일한 아이디로 재가입하실 수 없습니다.</li>
-			<li>3. 구인정보, 이력서 정보, 구인구직 활동내역, 유료서비스, e-머니가 모두 삭제됩니다.</li>
+            <li>1. Таны ашиглаж буй ID <strong><?=$member['mb_id'];?></strong>дахин ашиглах эсвэл сэргээх боломжгүй.</li>
+            <li>2. Дараа дахин бүртгүүлэхдээ ижил ID-гаар бүртгүүлэх боломжгүй.</li>
+            <li>3. Ажлын мэдээлэл, анкет, ажил хайх, төлбөртэй үйлчилгээ зэргийг бүгдийг нь устгана.</li>
 		</ol>
 	</div>
 	<div class="info4_con agree"><input type="checkbox" id="" name="left_agreement" value="Y">위 내용에 동의합니다.</div>
@@ -33,24 +33,24 @@ var member_left = function() {
 <section class="cont_box remove_con">
 	<ul class="info4_con">
 		<li class="row_con">
-			<label for="member_type">회원구분</label>
-			<span><?=$netfu_member->member_kind[$member['mb_type']];?></span>
-		</li>
-		<li class="row1">
-			<label for="id">회원아이디</label>
-			<span><?=$member['mb_id'];?></span>
-		</li>
-		<li class="row2">
-			<label for="pw2">비번확인<span class="check"></span></label>
-			<input type="password" id="pw2" name="mb_password" hname="비번확인" required maxlength="16">							
-		</li>
-		<li class="row3">
-			<label for="name">이메일<span class="check"></span></label>
-			<input type="text" id="name" name="mb_email" hname="이메일" required maxlength="">
-		</li>
-		<li class="row4">
-		  <fieldset>
-				<legend>탈퇴사유<span class="check"></span></legend>
+            <label for="member_type">Гишүүний төрөл</label>
+            <span><?=$netfu_member->member_kind[$member['mb_type']];?></span>
+        </li>
+        <li class="row1">
+            <label for="id">Гишүүний ID</label>
+            <span><?=$member['mb_id'];?></span>
+        </li>
+        <li class="row2">
+            <label for="pw2">Нууц үг батлах<span class="check"></span></label>
+            <input type="password" id="pw2" name="mb_password" required hname="Нууц үг" maxlength="16">
+        </li>
+        <li class="row3">
+            <label for="name">И-мэйл<span class="check"></span></label>
+            <input type="text" id="name" name="mb_email"  required hname="e-mail" maxlength="">
+        </li>
+        <li class="row4">
+            <fieldset>
+                <legend>Цуцлах шалтгаан<span class="check"></span></legend>
 				<ol>
 					<?php
 					if(is_array($_cate_['member_left_reason'])) { foreach($_cate_['member_left_reason'] as $k=>$v) {
@@ -66,7 +66,7 @@ var member_left = function() {
 </section>
 
 <div class="button_con">
-	<a href="#none;" class="bottom_btn01" onClick="member_left()">신청</a><a href="#none;" class="bottom_btn02">취소</a>
+	<a href="#none;" class="bottom_btn01" onClick="member_left()">Батлах</a><a href="#none;" class="bottom_btn02">Цуцлах</a>
 </div>
 
 </form>
