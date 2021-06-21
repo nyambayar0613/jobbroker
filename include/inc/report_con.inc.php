cc2865b09d5cf15ec177b1a019292bc684e11304
maralgua <bmaral0926@gmail.com>
2021-06-21 14:19:03 -0700

    орчуулга хийгдсэн.

--- include/inc/report_con.inc.php
+++ include/inc/report_con.inc.php
@@ -1,19 +1,19 @@
 <!-- 입사지원 내용보기 -->
 <div class="detail_ly cf" id="ly02">
-	<div class="detail_inner">
-		<div class="bx-top"><h2>면접제의 내용보기</h2>
-			<div class="btn-r btn-r2"><button id="close_ly" type="button">X</button></div>
-		</div>
-	<div class="content">
-		<h3><strong>(주)파이낸뷰</strong>에서 보내온 면접제의 내용입니다.</h3>
-		<p>
-			파이낸뷰에서 면접제의 합니다.<br>
-			회사에 방문하셔서 면접 보시기 바랍니다.
-		</p>
-	</div>
-	</div>
-	<div class="button_con">
-		<a href="#" class="bottom_btn07">지원하기<img src="images/icon_arrow_right3.png" alt="지원하기"></a>
+    <div class="detail_inner">
+        <div class="bx-top"><h2>Ярилцлагын агуулгыг үзэх</h2>
+            <div class="btn-r btn-r2"><button id="close_ly" type="button">X</button></div>
+        </div>
+        <div class="content">
+            <h3><strong>Санхүүгийн Вью ХХК</strong>-ийн илгээсэн ярилцлагын агуулга.</h3>
+            <p>
+                Financial View дээр ярилцлага өгөхийг санал болгож байна.<br>
+                Тус компани дээр очиж ярилцлага өгнө үү.
+            </p>
+        </div>
+    </div>
+    <div class="button_con">
+        <a href="#" class="bottom_btn07">Өргөдөл гаргах<img src="images/icon_arrow_right3.png" alt="Өргөдөл гаргах"></a>
 	</div>
 </div>
 <!-- //입사지원 내용보기 -->
