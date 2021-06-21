b32511024845a36f75163e110d5076eb92645712
maralgua <bmaral0926@gmail.com>
2021-06-21 14:25:20 -0700

    орчуулга хийгдсэн.

--- include/inc/resume_detail_tab1.inc.BKP.php
+++ include/inc/resume_detail_tab1.inc.BKP.php
@@ -10,26 +10,26 @@ $implode_tag_job_type = $re_info['max_job_type']>1 ? '<br/>' : ', ';
 ?>
 <div class="tab-box tab1-box" style="display:block;">
 <div class="resume_ct r_ct1 cf">
-	<h3>Хүсч буй ажлын нөхцөл</h3>
+	<h3>희망근무조건</h3>
 	<table>
 		<tr>
-			<th>Хүсч буй байршил</th>
+			<th>희망근무지</th>
 			<td><?=@implode($implode_tag_area, $re_info['area_val']);?></td>
 		</tr>
 		<tr>
-			<th>Хүсч буй ажлын төрөл</th>
+			<th>희망직종</th>
 			<td><?=@implode($implode_tag_job_type, $re_info['job_type_val']);?></td>
 		</tr>
 		<tr>
-			<th>Ажлын цаг</th>
+			<th>근무일시</th>
 			<td><?=$_wr_date[0];?>, <?=$wr_week[0];?>, <?=$wr_time[0];?>, <?=$re_row['wr_work_direct'] ? '즉시출근가능' : '';?></td>
 		</tr>
 		<tr>
-			<th>Цалин</th>
+			<th>희망급여</th>
 			<td><?=$re_info['pay_type'];?></td>
 		</tr>
 		<tr>
-			<th>Ажлын төрөл</th>
+			<th>근무형태</th>
 			<td><?=$re_info['work_type'];?></td>
 		</tr>
 	</table>
