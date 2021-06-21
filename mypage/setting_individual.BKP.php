6c7ca1a45cbb428646f1d0287163ba2ba23fb7f1
maralgua <bmaral0926@gmail.com>
2021-06-21 09:33:44 -0700

    орчуулга хийгдсэн.

--- mypage/setting_individual.BKP.php
+++ mypage/setting_individual.BKP.php
@@ -1,5 +1,5 @@
 <?php
-$head_title = "Санал болгох ажлын байр";
+$head_title = "맞춤구인정보";
 $page_code = 'mypage';
 include "../include/top.php";
 
@@ -40,8 +40,8 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 <section class="cont_box resume_list">
 	<div class="resume_list_con cf">
 		<ul class="list-tab">
-			<li class="tab01 <?=$_GET['code']=='list' ? 'active' : '';?>"><a href="<?=$_SEVRER['PHP_SELF'];?>?code=list&no=<?=$_GET['no'];?>">Санал болгох ажлын байр<span class="list_num"><?=number_format(count($custom_titles));?></span></a></li>
-			<li class="tab02 <?=$_GET['code']=='input' ? 'active' : '';?>"><a href="<?=$_SEVRER['PHP_SELF'];?>?code=input&no=<?=$_GET['no'];?>"> Өөрчлөх· засварлах</a></li>
+			<li class="tab01 <?=$_GET['code']=='list' ? 'active' : '';?>"><a href="<?=$_SEVRER['PHP_SELF'];?>?code=list&no=<?=$_GET['no'];?>">맞춤 구인정보<span class="list_num"><?=number_format(count($custom_titles));?></span></a></li>
+			<li class="tab02 <?=$_GET['code']=='input' ? 'active' : '';?>"><a href="<?=$_SEVRER['PHP_SELF'];?>?code=input&no=<?=$_GET['no'];?>">조건설정 · 수정</a></li>
 		</ul>
 		<ul class="search_area">
 			<li>
@@ -52,14 +52,14 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 						if(is_array($custom_titles)) { foreach($custom_titles as $k=>$v) {
 							$selected = $v['no']==$_GET['no'] ? 'selected' : '';
 						?>
-						<option value="<?=$v['no'];?>" <?=$selected;?>>[<?=$k+1;?>]<?=$v['wdate'];?> Хадгалах</option>
+						<option value="<?=$v['no'];?>" <?=$selected;?>>[<?=$k+1;?>]<?=$v['wdate'];?> 저장</option>
 						<?php
 						} }
 						?>
 					</select>
 				</div>
 				<div class="search_btn2">
-					<button type="button" onClick="save_search()"><img src="<?=NFE_URL;?>/images/search_icon.png" alt="Хайх">Хайх</button>
+					<button type="button" onClick="save_search()"><img src="<?=NFE_URL;?>/images/search_icon.png" alt="검색">검색</button>
 				</div>
 			</li>
 		</ul>
@@ -77,7 +77,7 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 			?>
 			<ul class="list_con">
 				<li class="col2 none">
-					<div class="list_txt2"><img src="<?=NFE_URL;?>/images/info.png" alt="">Санал болгох ажлын байр байхгүй байна.</div>
+					<div class="list_txt2"><img src="<?=NFE_URL;?>/images/info.png" alt="">맞춤 구인정보가 없습니다.</div>
 				</li>
 			</ul>
 			<?php
@@ -97,7 +97,7 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 						$area_txt = $area_arr[0].' '.$area_arr[1];
 
 						// : 급여
-						$wr_pay = number_format($row['wr_pay'])."төгрөг";
+						$wr_pay = number_format($row['wr_pay'])."원";
 						$wr_pay_type = $category_control->get_categoryCode($row['wr_pay_type']);
 						$wr_pay_txt = ($row['wr_pay_conference']) ? '<i>'.$alba_user_control->pay_conference[$row['wr_pay_conference']].'</i>' : '<i>'.$wr_pay_type['name'].'</i> <em>'.$wr_pay.'</em>';
 
@@ -114,7 +114,7 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 					?>
 				</a></li>
 				<li class="col3">
-					<div class="list_btn list_btn3" onClick="netfu_mjob.scrap('alba', '<?=$row['no'];?>')"><button type="button">Scrab <img src="<?=NFE_URL;?>/images/scrap_icon2.png"><!--<img src="<?=NFE_URL;?>/images/scrap_icon1.png">--></button></div>
+					<div class="list_btn list_btn3" onClick="netfu_mjob.scrap('alba', '<?=$row['no'];?>')"><button type="button">스크랩 <img src="<?=NFE_URL;?>/images/scrap_icon2.png"><!--<img src="<?=NFE_URL;?>/images/scrap_icon1.png">--></button></div>
 					<div class="button_group"><div class="list_btn list_btn4 bt-online _btn requisition_btn" no="<?=$row['no'];?>" k="online"><button type="button">즉시지원</button></div></div>
 				</li>
 			</ul>
@@ -170,11 +170,11 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 				<!-- 맞춤조건1 -->
 				<tr>
 					<th class="sch_hd">
-                        <div>Ажлын төрөл</div>
-                    </th>
-                    <td class="sch_td1">
-                        <select name="wr_job_type0" sel="1" type="job_type" val="<?=$_job_type_arr[1];?>" onChange="netfu_util1.ajax_cate(this, 'job_type', 1)" put="wr_job_type1_id" auto_none>
-                            <option value="">Ажлын төрөл 1</option>
+						<div>업직종</div>
+					</th>
+					<td class="sch_td1">
+						<select name="wr_job_type0" sel="1" type="job_type" val="<?=$resume_job_type[0][1];?>" onChange="netfu_util1.ajax_cate(this, 'job_type', 1)" put="wr_job_type1_id" auto_none>
+							<option value="">업직종1차</option>
 							<?php
 							if(is_array($_cate_['job_type'])) { foreach($_cate_['job_type'] as $k=>$v) {
 								$selected = $v['code']==$resume_job_type[0][0] ? 'selected' : '';
@@ -187,7 +187,7 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 					</td>
 					<td class="sch_td2">
 						<select id="wr_job_type1_id" name="wr_job_type1" sel="2" type="job_type" val="<?=$resume_job_type[0][1];?>" onChange="netfu_util1.ajax_cate(this, 'job_type', 2)" put="wr_job_type2_id" auto_none>
-							<option value="">Ажлын төрөл 2</option>
+							<option value="">업직종2차</option>
 							<?php
 							if(is_array($job_type1_arr)) { foreach($job_type1_arr as $k=>$v) {
 								$selected = $v['code']==$resume_job_type[0][1] ? 'selected' : '';
@@ -200,7 +200,7 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 					</td>
 					<td class="sch_td3">
 						<select id="wr_job_type2_id" name="wr_job_type2">
-							<option value="">Ажлын төрөл 3</option>
+							<option value="">업직종3차</option>
 							<?php
 							if(is_array($job_type2_arr)) { foreach($job_type2_arr as $k=>$v) {
 								$selected = $v['code']==$resume_job_type[0][2] ? 'selected' : '';
@@ -215,38 +215,44 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 				<!-- 맞춤조건2 -->
 				<tr>
 					<th class="sch_hd">
-                        <div>Ажлын байршил</div>
-                    </th>
-                    <td class="sch_td1">
-                        <select name="wr_area0" sel="1" type="area" val="<?=$_area_arr[1];?>" put="wr_area1_id" onChange="netfu_util1.ajax_cate(this, 'area', 1)">
-                            <option value="">Хот·дүүрэг</option>
-                            <?php
-                            if(is_array($_cate_['area'])) { foreach($_cate_['area'] as $k=>$v) {
-                                $selected = $_area_arr[0]==$v['code'] ? 'selected' : '';
-                                ?>
-                                <option value="<?=$v['code'];?>" <?=$selected;?>><?=$v['name'];?></option>
-                                <?php
-                            } }
-                            ?>
-                        </select>
-                    </td>
-                    <td class="sch_td2">
-                        <select id="wr_area1_id" name="wr_area1">
-                            <option value="">хороо·тоот·гудамж</option>
-                        </select>
-                    </td>
-                    <td class="sch_td3">
-                        <input type="checkbox" id="homejob" name="wr_home_work" value="1" <?php echo ($get_custom['wr_home_work']) ? 'checked' : '';?>><label for="homejob">Гэрээсээ ажиллах</label>
-                    </td>
-                </tr>
-                <!-- 검색유형3 -->
-                <tr>
-                    <th class="sch_hd">
-                        <div>Ажиллах хугацаа</div>
-                    </th>
-                    <td class="sch_td1">
-                        <select name="wr_date">
-                            <option value="">Ажиллах хугацаа</option>
+						<div>근무지</div>
+					</th>
+					<td colspan="3">
+						<table class="tb2">
+							<tr>
+								<td class="sch_td1">
+									<select name="wr_area0" sel="1" type="area" val="<?=$resume_area[0][1];?>" put="wr_area1_id" onChange="netfu_util1.ajax_cate(this, 'area', 1)">
+									<option value="">시·도</option>
+									<?php
+									if(is_array($_cate_['area'])) { foreach($_cate_['area'] as $k=>$v) {
+										$selected = $resume_area[0][0]==$v['code'] ? 'selected' : '';
+									?>
+									<option value="<?=$v['code'];?>" <?=$selected;?>><?=$v['name'];?></option>
+									<?php
+									} }
+									?>
+									</select>
+								</td>
+								<td class="sch_td2">
+									<select id="wr_area1_id" name="wr_area1">
+										<option value="">시·군·구</option>
+									</select>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<!-- 맞춤조건3 -->
+				<tr>
+					<th class="sch_hd">
+						<div>근무일시</div>
+					</th>
+					<td colspan="3">
+						<table class="tb2">
+							<tr>
+								<td class="tb2_sch_td1">
+									<select name="wr_date">
+										<option value="">근무기간</option>
 										<?php
 										if(is_array($_cate_['alba_date'])) { foreach($_cate_['alba_date'] as $k=>$v) {
 											$selected = $get_custom['wr_date']==$v['code'] ? 'selected' : '';
@@ -259,7 +265,7 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 								</td>
 								<td class="tb2_sch_td2">
 									<select name="wr_week">
-                                        <option value="">Ажлын өдөр</option>
+										<option value="">근무요일</option>
 										<?php
 										if(is_array($_cate_['alba_week'])) { foreach($_cate_['alba_week'] as $k=>$v) {
 											$selected = $get_custom['wr_week']==$v['code'] ? 'selected' : '';
@@ -274,35 +280,35 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 							<tr>
 								<td class="tb2_sch_td1 time_slt_td" colspan="2">
 									<select name="wr_stime[]">
-										<option value="">Сонгох</option>
+										<option value="">선택</option>
 										<?php for($i=0;$i<=23;$i++){ ?>
-										<option value="<?php echo sprintf('%02d',$i);?>" <?php echo ($wr_stime[0]&&$wr_stime[0]==$i)?'selected':'';?>><?php echo sprintf('%02d',$i);?>цаг</option>
+										<option value="<?php echo sprintf('%02d',$i);?>" <?php echo ($wr_stime[0]&&$wr_stime[0]==$i)?'selected':'';?>><?php echo sprintf('%02d',$i);?>시</option>
 										<?php } ?>
 									</select>
 									<select name="wr_stime[]">
-										<option value="">Сонгох</option>
+										<option value="">선택</option>
 										<?php for($i=0;$i<=5;$i++){?>
-										<option value="<?php echo $i;?>0" <?php echo ($wr_stime[1]==$i.'0')?'selected':'';?>><?php echo $i;?>0мин</option>
+										<option value="<?php echo $i;?>0" <?php echo ($wr_stime[1]==$i.'0')?'selected':'';?>><?php echo $i;?>0분</option>
 										<?php } ?>
 									</select>
 									<span>~</span>
 									<select name="wr_etime[]">
-										<option value="">Сонгох</option>
+										<option value="">선택</option>
 										<?php for($i=0;$i<=23;$i++){ ?>
-										<option value="<?php echo sprintf('%02d',$i);?>" <?php echo ($wr_etime[0]&&$wr_etime[0]==$i)?'selected':'';?>><?php echo sprintf('%02d',$i);?>цаг</option>
+										<option value="<?php echo sprintf('%02d',$i);?>" <?php echo ($wr_etime[0]&&$wr_etime[0]==$i)?'selected':'';?>><?php echo sprintf('%02d',$i);?>시</option>
 										<?php } ?>
 									</select>
 									<select name="wr_etime[]">
-										<option value="">Сонгох</option>
+										<option value="">선택</option>
 										<?php for($i=0;$i<=5;$i++){?>
-										<option value="<?php echo $i;?>0" <?php echo ($wr_etime[1]==$i.'0')?'selected':'';?>><?php echo $i;?>0мин</option>
+										<option value="<?php echo $i;?>0" <?php echo ($wr_etime[1]==$i.'0')?'selected':'';?>><?php echo $i;?>0분</option>
 										<?php } ?>
 									</select>
 								</td>
 							</tr>
 							<tr>
 								<td class="tb2_sch_td1 cst" colspan="2">
-									<input type="checkbox" id="time_adj" name="wr_time_conference" value="1" <?=$get_custom['wr_time_conference'] ? 'checked' : '';?>><label for="time_adj">Цаг тохирох</label>
+									<input type="checkbox" id="time_adj" name="wr_time_conference" value="1" <?=$get_custom['wr_time_conference'] ? 'checked' : '';?>><label for="time_adj">시간협의</label>
 								</td>
 							</tr>
 						</table>
@@ -311,39 +317,39 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 				<!-- 맞춤조건4 -->
 				<tr>
 					<th class="sch_hd">
-						<div>Хүйс сонгох</div>
+						<div>성별선택</div>
 					</th>
 					<td class="sch_td2" colspan="3">
 						<fieldset>
-							<legend>Хүйс</legend>
-							<label for="no-gender"><input type="radio" id="no-gender" name="wr_gender" value="0" <?=$get_custom['wr_gender']==='0' ? 'checked' : '';?>>Хүйс хамааралгүй</label>
-							<label for="male"><input type="radio" id="male" name="wr_gender" value="1" <?=$get_custom['wr_gender']==1 ? 'checked' : '';?>>Эр</label>
-							<label for="female"><input type="radio" id="female" name="wr_gender" value="2" <?=$get_custom['wr_gender']==2 ? 'checked' : '';?>>Эм</label>
+							<legend>성별선택</legend>
+							<label for="no-gender"><input type="radio" id="no-gender" name="wr_gender" value="0" <?=$get_custom['wr_gender']==='0' ? 'checked' : '';?>>성별무관</label>
+							<label for="male"><input type="radio" id="male" name="wr_gender" value="1" <?=$get_custom['wr_gender']==1 ? 'checked' : '';?>>남자</label>
+							<label for="female"><input type="radio" id="female" name="wr_gender" value="2" <?=$get_custom['wr_gender']==2 ? 'checked' : '';?>>여자</label>
 						</fieldset>
 					</td>
 				</tr>
 				<!-- 맞춤조건5 -->
 				<tr>
 					<th class="sch_hd">
-						<div>Нас</div>
+						<div>연령</div>
 					</th>
 					<td class="sch_td2" colspan="3">
 						<fieldset>
-							<legend>Сонгох</legend>
-							<input type="radio" name="wr_age_limit" value="0" <?=$get_custom['wr_age_limit']==='0' ? 'checked' : '';?>>Хамааралгүй<br>
-							<input type="radio" name="wr_age_limit" value="1" <?=$get_custom['wr_age_limit']==='1' ? 'checked' : '';?>>Насны хязгаар
-							<input type="text" name="wr_sage" value="<?=$wr_age[0];?>">наснаас дээш ~<input type="text" name="wr_eage" value="<?=$wr_age[1];?>"> наснаас доош
+							<legend>연령선택</legend>
+							<input type="radio" name="wr_age_limit" value="0" <?=$get_custom['wr_age_limit']==='0' ? 'checked' : '';?>>연령무관<br>
+							<input type="radio" name="wr_age_limit" value="1" <?=$get_custom['wr_age_limit']==='1' ? 'checked' : '';?>>연령제한
+							<input type="text" name="wr_sage" value="<?=$wr_age[0];?>">세 이상 ~<input type="text" name="wr_eage" value="<?=$wr_age[1];?>"> 세 이하
 						</fieldset>
 					</td>
 				</tr>
 				<!-- 맞춤조건6 -->
 				<tr>
 					<th class="sch_hd">
-						<div>Ажлын төрөл</div>
+						<div>근무형태</div>
 					</th>
 					<td class="sch_td2" colspan="3">
 						<fieldset>
-							<legend>Сонгох</legend>
+							<legend>근무형태선택</legend>
 							<?php
 							if(is_array($_cate_['work_type'])) { foreach($_cate_['work_type'] as $k=>$v) {
 								$checked = @in_array($v['code'], $wr_work_type) ? 'checked' : '';
@@ -358,25 +364,25 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 				<!-- 맞춤조건7 -->
 				<tr>
 					<th class="sch_hd">
-						<div>Туршлага</div>
+						<div>경력</div>
 					</th>
 					<td class="sch_td2" colspan="3">
 						<fieldset>
-							<legend>Сонгох</legend>
-							<input type="radio" name="wr_career" value="0">Туршлага хамааралгүй
-							<input type="radio" name="wr_career" value="1">Шинэ ажилтан
-							<input type="radio" name="wr_career" value="2">Туршлагатай
+							<legend>경력선택</legend>
+							<input type="radio" name="wr_career" value="0">무관
+							<input type="radio" name="wr_career" value="1">신입
+							<input type="radio" name="wr_career" value="2">경력
 						</fieldset>
 					</td>
 				</tr>
 				<!-- 맞춤조건8 -->
 				<tr>
 					<th class="sch_hd">
-						<div>Боловсрол</div>
+						<div>학력</div>
 					</th>
 					<td class="sch_td2" colspan="3">
 						<select name="wr_ability">
-							<option value="">Сонгох</option>
+							<option value="">학력선택</option>
 							<?php
 							if(is_array($_cate_['job_ability'])) { foreach($_cate_['job_ability'] as $k=>$v) {
 							?>
@@ -399,7 +405,7 @@ include NFE_PATH.'/include/inc/my_resume_count.inc.php';
 if($_GET['code']=='input') {
 ?>
 <div class="button_con">
-	<a href="#none;" class="bottom_btn01" onClick="document.forms['fwrite'].submit()">Хадгалах</a><a href="javascript:document.forms['fwrite'].reset()" class="bottom_btn02">Цуцлах</a>
+	<a href="#none;" class="bottom_btn01" onClick="document.forms['fwrite'].submit()">저장</a><a href="javascript:document.forms['fwrite'].reset()" class="bottom_btn02">취소</a>
 </div>
 <?php }
 
