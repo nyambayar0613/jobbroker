7e5e1e522c99d97a716cd140591d9414b612275b
maralgua <bmaral0926@gmail.com>
2021-06-21 10:09:46 -0700

    орчуулга хийгдсэн.

--- mypage/tax.BKP.php
+++ mypage/tax.BKP.php
@@ -1,6 +1,6 @@
 <?php
 $page_code = 'mypage';
-$head_title = $menu_text = "Бэлэн мөнгөний баримт олгох";
+$head_title = $menu_text = "현금영수증 발행신청";
 $menu_code = 'text';
 include "../include/top.php";
 
@@ -16,20 +16,20 @@ var email_put = function(el) {
 <form name="fwrite" action="../regist.php" method="post">
 <input type="hidden" name="mode" value="tax_write" />
 <section class="cont_box receipt_con">
-	<h2>Бэлэн мөнгөний баримт хүсэлт</h2>
+	<h2>현금영수증 발생신청</h2>
 	<ol class="info3_con">
-		<li>1. Төлбөртэй үйлчилгээнд хамрагдаж, баримт олгох хүсэлт гаргасны дараа олгогдоно. Гэхдээ төлбөрийг бүрэн хийж дууссаны дараа бэлэн мөнгөний баримт авах боломжтой.</li>
-		<li style="margin-bottom:0">2. <span class="check"></span>оруулах шаардлагатай.</li>
+		<li>1. 유료서비스 신청후 현금영수증 발행신청시 발행해드리고 있습니다. 단, 결제완료 후 현금영수증이 발행되오니 이 점 숙지하시기 바랍니다.</li>
+		<li style="margin-bottom:0">2. <span class="check"></span>표시는 필수 입력사항입니다.</li>
 	</ol>
 	<ul class="info3_con">
 		<li class="row1">
-			<label for="resume_tit">Хүсэлт гаргагчын нэр<span class="check"></span></label>
+			<label for="resume_tit">신청자명<span class="check"></span></label>
 			<input type="text" id="resume_tit" name="wr_name" value="<?=$member['mb_name'];?>">
 		</li>  
 		<li class="row3">
 			<fieldset>
-				<legend>Утсаны дугаар<span class="check"></span></legend>
-				<select name="wr_hphone[]" hname="Улсын дугаар">
+				<legend>휴대폰<span class="check"></span></legend>
+				<select name="wr_hphone[]" hname="국번">
 				<?php echo $hp_num_option; ?>
 				</select>
 				<p>-</p><input type="tel" size="4" maxlength="4" name="wr_hphone[]" value="<?=$_hphone[1];?>" class="cel1 phone1">
@@ -38,34 +38,34 @@ var email_put = function(el) {
 		</li>
 		<li class="row4">
 			<fieldset>
-			<legend>И-мэйл<span class="check"></span></legend>
+			<legend>이메일<span class="check"></span></legend>
 				<input type="text" name="wr_email[]" value="<?=$_email[0];?>" class="email">
 				<p>@</p><input type="text" name="wr_email[]" value="<?=$_email[1];?>" class="email">
 				<select onChange="email_put(this)">
-					<option value="">мэйл</option>
+					<option value="">직접입력</option>
 					<?php echo $email_option; ?>
 				</select>
 			</fieldset>
 		</li>
 		<li class="row5">
 			<fieldset>
-				<legend>Төлбөрийн огноо<span class="check"></span></legend>
+				<legend>결제일자<span class="check"></span></legend>
 				<input type="text" name="wr_pay_date" readOnly class="datepicker_inp paymindt" readOnly value="<?=$_get['pay_sdate'];?>">
 			</fieldset>
 		</li>
 		<li class="row6">
-			<label for="pay">Төлбөрийн хэмжээ</label>
+			<label for="pay">결제금액</label>
 			<input class="paymin" type="text" name="wr_price">원
 		</li>
 		<li class="row7">
-			<label>Агуулга</label>
+			<label>내용</label>
 			<textarea name="wr_content"></textarea>
 		</li>
 	</ul>
 </section>
 
 <div class="button_con">
-	<a href="#none;" class="bottom_btn01" onClick="netfu_util1.ajax_submit(document.forms['fwrite'])">Өргөдөл гаргах </a><a href="#none;" class="bottom_btn02" onClick="document.forms['fwrite'].reset()">Цуцлах</a>
+	<a href="#none;" class="bottom_btn01" onClick="netfu_util1.ajax_submit(document.forms['fwrite'])">신청하기</a><a href="#none;" class="bottom_btn02" onClick="document.forms['fwrite'].reset()">취소</a>
 </div>
 </form>
 
