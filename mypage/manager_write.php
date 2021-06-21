56e4ad28a233134efb299e55313135479c0915e0
maralgua <bmaral0926@gmail.com>
2021-06-21 16:52:08 -0700

    орчуулга хийгдсэн.

--- mypage/manager_write.php
+++ mypage/manager_write.php
@@ -29,49 +29,49 @@ include NFE_PATH.'/include/inc/my_company_count.inc.php';
 <input type="hidden" name="mode" value="company_manager_write" />
 <input type="hidden" name="no" value="<?=$manager_row['no'];?>" />
 <section id="manager_write" class="cont_box add2_info">
-	<h2>구인담당자 정보 입력</h2>
+	<h2>Ажилд авах хүний ​​мэдээллийг оруулна уу</h2>
 	<ul class="info3_con">
 		<li class="row1">
-			<label for="manager_name">담당자명<span class="check"></span></label>
-			<input type="text" name="wr_name" hname="담당자명" required value="<?=$manager_row['wr_name'];?>">
+			<label for="manager_name">Нэр<span class="check"></span></label>
+			<input type="text" name="wr_name" hname="Нэр" required value="<?=$manager_row['wr_name'];?>">
 		</li>
 		<li class="row2">
 			<fieldset>
-				<legend>휴대폰</legend>
+				<legend>Утасны дугаар</legend>
 				<select name="wr_hphone[]">
 				<?php echo $hp_num_option; ?>
 				</select>
-				<p>-</p><input type="tel" size="4" maxlength="4" name="wr_hphone[]" hname="휴대폰"  value="<?=$_hphone[1];?>" class="cel1 phone1">
-				<p>-</p><input type="tel" size="4" maxlength="4" name="wr_hphone[]" hname="휴대폰"  value="<?=$_hphone[2];?>" class="cel2 ">
+				<p>-</p><input type="tel" size="4" maxlength="4" name="wr_hphone[]" hname="Утасны дугаар"  value="<?=$_hphone[1];?>" class="cel1 phone1">
+				<p>-</p><input type="tel" size="4" maxlength="4" name="wr_hphone[]" hname="Утасны дугаар"  value="<?=$_hphone[2];?>" class="cel2 ">
 			</fieldset>
 		</li>
 		<li class="row3">
 			<fieldset>
-				<legend>전화번호<span class="check"></span></legend>
+				<legend>Холбоо барих<span class="check"></span></legend>
 					<select name="wr_phone[]">
 					<?php echo $tel_num_option; ?>
 					</select>
-					<p>-</p><input type="tel" size="4" maxlength="4" name="wr_phone[]" hname="전화번호" required value="<?=$_phone[1];?>" class="tel1 phone2">
-					<p>-</p><input type="tel" size="4" maxlength="4" name="wr_phone[]" hname="전화번호" required value="<?=$_phone[2];?>" class="tel2 ">
+					<p>-</p><input type="tel" size="4" maxlength="4" name="wr_phone[]" hname="Холбоо барих" required value="<?=$_phone[1];?>" class="tel1 phone2">
+					<p>-</p><input type="tel" size="4" maxlength="4" name="wr_phone[]" hname="Холбоо барих" required value="<?=$_phone[2];?>" class="tel2 ">
 			</fieldset>
 		</li>
 		<li class="row4">
 			<fieldset>
-				<legend>팩스번호</legend>
+				<legend>Шуудангын дугаар</legend>
 					<select name="wr_fax[]">
 					<?php echo $fax_num_option; ?>
 					</select>
-					<p>-</p><input type="tel" size="4" maxlength="4" name="wr_fax[]" hname="팩스번호"  value="<?=$_fax[1];?>" class="fax1 phone2">
-					<p>-</p><input type="tel" size="4" maxlength="4" name="wr_fax[]" hname="팩스번호"  value="<?=$_fax[2];?>" class="fax2 ">
+					<p>-</p><input type="tel" size="4" maxlength="4" name="wr_fax[]" hname="Шуудангын дугаар"  value="<?=$_fax[1];?>" class="fax1 phone2">
+					<p>-</p><input type="tel" size="4" maxlength="4" name="wr_fax[]" hname="Шуудангын дугаар"  value="<?=$_fax[2];?>" class="fax2 ">
 			</fieldset>
 		</li>
 		<li class="row5">
 			<fieldset>
-				<legend>이메일<span class="check"></span></legend>
-				<input type="text" name="wr_email[]" value="<?=$_email[0];?>" hname="이메일" required class="email">
-				<p>@</p><input type="text" name="wr_email[]" value="<?=$_email[1];?>" hname="이메일" required class="email">
+				<legend>И-мэйл<span class="check"></span></legend>
+				<input type="text" name="wr_email[]" value="<?=$_email[0];?>" hname="И-мэйл" required class="И-мэйл">
+				<p>@</p><input type="text" name="wr_email[]" value="<?=$_email[1];?>" hname="И-мэйл" required class="И-мэйл">
 				<select onChange="netfu_util1.email_put(this)">
-				<option value="">직접입력</option>
+				<option value="">мэйл</option>
 				<?php echo $email_option; ?>
 				</select>
 			</fieldset>
@@ -79,7 +79,7 @@ include NFE_PATH.'/include/inc/my_company_count.inc.php';
 </section>
 
 <div class="button_con">
-	<a href="#none;" class="bottom_btn01" onClick="netfu_util1.ajax_submit(document.forms['fwrite'])"><?=$manager_row ? '수정' : '저장';?></a><a href="#" class="bottom_btn02" onClick="document.forms['fwrite'].reset()">취소</a>
+	<a href="#none;" class="bottom_btn01" onClick="netfu_util1.ajax_submit(document.forms['fwrite'])"><?=$manager_row ? 'Өөрчлөх' : 'Хадгадах';?></a><a href="#" class="bottom_btn02" onClick="document.forms['fwrite'].reset()">Цуцлах</a>
 </div>
 </form>
 
