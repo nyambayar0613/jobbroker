4452ddef1d95aeb523c48708c03fbe4c0fcc11a2
maralgua <bmaral0926@gmail.com>
2021-06-21 16:49:24 -0700

    орчуулга хийгдсэн.

--- mypage/manager_info.php
+++ mypage/manager_info.php
@@ -1,5 +1,5 @@
 <?php
-$head_title = "담당자관리";
+$head_title = "Хариуцагч удирдах";
 $page_code = 'mypage';
 include_once "../include/top.php";
 
@@ -21,9 +21,9 @@ include NFE_PATH.'/include/inc/my_company_count.inc.php';
 <section class="cont_box resume_list" style="box-shadow:none;border-bottom:0">
 	<div class="resume_list_con cf">
 		<ul class="list-tab">
-			<li class="tab01 active"><a href="#none;">구인담당자 관리<span class="list_num"><?=number_format($_nums);?></span></a></li>
+			<li class="tab01 active"><a href="#none;">Хариуцагч удирдах<span class="list_num"><?=number_format($_nums);?></span></a></li>
 			<!-- <li class="tab02"><a href="#">마감된 구인공고<span class="list_num">1</span></a></li> -->
-			<li class="add_bt"><a href="./manager_write.php">구인담당자 추가 +</a></li>
+			<li class="add_bt"><a href="./manager_write.php">Ажилд авах хүн нэмэх +</a></li>
 		</ul>
 		<?php
 		switch($_nums<=0) {
@@ -31,7 +31,7 @@ include NFE_PATH.'/include/inc/my_company_count.inc.php';
 		?>
 		<ul class="list_con" style="border-bottom:1px solid #dee3eb">
 			<li class="col2 none">
-				<div class="list_txt2"><img src="images/info.png" alt="">등록된 정보가 없습니다.</div>
+				<div class="list_txt2"><img src="images/info.png" alt="">Бүртгэлтэй мэдээлэл байхгүй байна.</div>
 			</li>
 		</ul>
 		<?php
@@ -46,30 +46,30 @@ include NFE_PATH.'/include/inc/my_company_count.inc.php';
 			<li class="col2">
 				<table class="list_con2">
 					<tr>
-						<th>담당자명</th>
+						<th>Хариуцсан хүний нэр</th>
 						<td class="company"><?=strip_tags(stripslashes($row['wr_name']));?></td>
 					</tr>
 					<tr>
-						<th>이메일</th>
+						<th>И-мэйл</th>
 						<td><?=strip_tags(stripslashes($row['wr_email']));?></td>
 					</tr>
 					<tr>
-						<th>전화번호</th>
+						<th>Холбоо барих</th>
 						<td><?=strip_tags(stripslashes($row['wr_phone']));?></td>
 					</tr>
 					<?php
 					if($row['wr_hphone']) {
 					?>
 					<tr>
-						<th>휴대폰</th>
+						<th>Утасны дугаар</th>
 						<td><?=strip_tags(stripslashes($row['wr_hphone']));?></td>
 					</tr>
 					<?php }?>
 				</table>
 			</li>
 			<li class="col3 col3-1">
-				<div class="list_btn list_btn1 list_btn2-1"><button type="button" onClick="netfu_util1.delete_func('<?=$row['no'];?>', 'company_manager')">삭제</button></div>
-				<div class="list_btn list_btn1 list_btn2-1"><a href="./manager_write.php?no=<?=$row['no'];?>"  class="btn_a" style="color:#3694ed"><button type="button">수정</button></a></div>
+				<div class="list_btn list_btn1 list_btn2-1"><button type="button" onClick="netfu_util1.delete_func('<?=$row['no'];?>', 'company_manager')">Устгах</button></div>
+				<div class="list_btn list_btn1 list_btn2-1"><a href="./manager_write.php?no=<?=$row['no'];?>"  class="btn_a" style="color:#3694ed"><button type="button">Засварлах</button></a></div>
 			</li>
 		</ul>
 		<?php
@@ -82,7 +82,7 @@ include NFE_PATH.'/include/inc/my_company_count.inc.php';
 </form>
 
 <div class="button_con button_con3">
-	<a href="#" class="bottom_btn03" onClick="netfu_util1.delete_select_func(document.forms['flist'])">선택삭제</a>
+	<a href="#" class="bottom_btn03" onClick="netfu_util1.delete_select_func(document.forms['flist'])">Сонголт устгах</a>
 </div>
 
 
