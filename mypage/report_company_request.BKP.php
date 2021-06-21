97dec06b3ddc6666b121a7b5e1879b526bf59f50
maralgua <bmaral0926@gmail.com>
2021-06-21 08:36:25 -0700

    орчуулга хийгдсэн.

--- mypage/report_company_request.BKP.php
+++ mypage/report_company_request.BKP.php
@@ -1,5 +1,5 @@
 <?php
-$head_title = "Ажилд орохыг хүссэн байгууллагууд";
+$head_title = "입사지원요청기업";
 $page_code = 'mypage';
 include "../include/top.php";
 
@@ -29,7 +29,7 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 <section class="cont_box resume_list">
 	<div class="resume_list_con cf">
 		<ul class="list-tab">
-			<li class="tab01 active"><a href="#none;">Ажилд орохыг хүссэн байгууллагууд<span class="list_num"><?=number_format($total['c']);?></span></a></li>
+			<li class="tab01 active"><a href="#none;">입사지원요청기업<span class="list_num"><?=number_format($total['c']);?></span></a></li>
 		</ul>
 
 		<?php
@@ -38,7 +38,7 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 		?>
 		<ul class="list_con">
 			<li class="col2 none">
-				<div class="list_txt2"><img src="<?=NFE_URL;?>/images/info.png" alt="">Ажилд орохыг санал болгож буй компани байхгүй байна.</div>
+				<div class="list_txt2"><img src="<?=NFE_URL;?>/images/info.png" alt="">입사제의기업이 없습니다.</div>
 			</li>
 		</ul>
 		<?php
@@ -57,7 +57,7 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 			?>
 			<li class="col1"><input type="checkbox" id="" name="chk[]" value="<?=$row['no'];?>"></li>
 			<li class="col2"><a href="<?=NFE_URL;?>/job/detail.php?no=<?=$row['wr_employ'];?>">
-				<div class="company_name"><?=$get_alba ? strip_tags(stripslashes($get_alba['wr_company_name'])) : 'Устгагдсан мэдээлэл.';?></div>
+				<div class="company_name"><?=$get_alba ? strip_tags(stripslashes($get_alba['wr_company_name'])) : '삭제된 정보입니다.';?></div>
 				<div class="list_txt"><?=strip_tags(stripslashes($get_alba['wr_subject']));?></div>
 				<div class="list_etc">
 					<span><?=$pay_type;?> <em><?=$wr_pay;?></em></span><span><?php
@@ -69,7 +69,7 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 					<span><?=$alba_user_control->gender_val[$get_alba['wr_gender']];?></span>
 					<span><?php
 					if($get_alba['wr_time_conference']){	// 시간 협의
-						$wr_time = "Цаг тохирох";
+						$wr_time = "시간협의";
 					} else {
 						$wr_time = $get_alba['wr_stime'] . " ~ " . $get_alba['wr_etime'];	// 근무시간
 					}
@@ -78,12 +78,12 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 					<span><?=$job_info['volume_text'];?></span>
 				</div>
 				<div class="list_etc">
-					<span>= Илгээсэн огноо : <?=$netfu_util->get_date('dot', $row['wdate']);?></span>
+					<span>발송일 : <?=$netfu_util->get_date('dot', $row['wdate']);?></span>
 				</div>
 			</a></li>
 			<li class="col3">
 				<div class="list_btn list_btn3">
-					<button type="button" id="show_ly" onClick="get_info_func(this, '<?=$row['no'];?>');"><img src="<?=NFE_URL;?>/images/folder_icon.png" alt="Агуулгыг үзэх" class="folder_icon">Дэлгэрэнгүй үзэх</button>
+					<button type="button" id="show_ly" onClick="get_info_func(this, '<?=$row['no'];?>');"><img src="<?=NFE_URL;?>/images/folder_icon.png" alt="입사제의요청 내용보기" class="folder_icon">내용보기</button>
 				</div>
 				<?
 				/*
@@ -103,7 +103,7 @@ $paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$t
 </section>
 
 <div class="button_con button_con3">
-	<a href="javascript:netfu_mjob.receive_request_all_delete()" class="bottom_btn03">Устгах</a>
+	<a href="javascript:netfu_mjob.receive_request_all_delete()" class="bottom_btn03">삭제</a>
 </div>
 </form>
 
---
+++ mypage/report_individual.BKP.php
@@ -0,0 +1,105 @@
+<?php
+$head_title = "입사지원관리";
+$page_code = 'mypage';
+include "../include/top.php";
+$_GET['code'] = $_GET['code'] ? $_GET['code'] : 'online';
+
+// : 개수정보
+$_my_count['receive_online'] = sql_fetch("select count(*) as c from alice_receive where `type`='become_online' and `wr_id`='".$member['mb_id']."'");
+$_my_count['receive_email'] = sql_fetch("select count(*) as c from alice_receive where `type`='become_email' and `wr_id`='".$member['mb_id']."'");
+
+// : 쿼리문
+$_limit = 10;
+$_GET['page'] = $_GET['page'] ? $_GET['page'] : 1;
+$_where = " and `wr_id`='".$member['mb_id']."' and `is_delete_indi`=0";
+$q = "alice_receive where `type`='become_".addslashes($_GET['code'])."' ".$_where;
+$_start = $netfu_util->_paging_start($_GET['page'], $_limit);
+$query = sql_query("select * from ".$q." order by `no` desc limit ".$_start.", ".$_limit);
+$total = sql_fetch("select count(*) as c from ".$q);
+$list_num = mysql_num_rows($query);
+$paging = $netfu_util->_paging_(array('var'=>'page', 'num'=>$_limit, 'total'=>$total['c']));
+?>
+<script type="text/javascript" src="<?=NFE_URL;?>/_helpers/_js/mjob.class.js?time=<?=time();?>"></script>
+<section class="cont_box detail_con">
+<?php
+include NFE_PATH.'/include/inc/member_info.inc.php';
+
+include NFE_PATH.'/include/inc/my_resume_count.inc.php';
+?>
+</section>
+
+
+<form name="flist">
+<section class="cont_box resume_list">
+	<div class="resume_list_con cf">
+		<ul class="list-tab">
+			<li class="tab01 <?=$_GET['code']=='online' ? 'active' : '';?>"><a href="<?=$_SERVER['PHP_SELF'];?>?code=online">온라인 지원현황<span class="list_num"><?=number_format($_my_count['receive_online']['c']);?></span></a></li>
+			<li class="tab02 <?=$_GET['code']=='email' ? 'active' : '';?>"><a href="<?=$_SERVER['PHP_SELF'];?>?code=email">이메일 지원현황<span class="list_num"><?=number_format($_my_count['receive_email']['c']);?></span></a></li>
+		</ul>
+		
+
+		<?php
+		switch($total['c']<=0) {
+			case true:
+		?>
+		<ul class="list_con">
+			<li class="col2 none">
+				<div class="list_txt2"><img src="<?=NFE_URL;?>/images/info.png" alt="">등록된 내용이 없습니다.</div>
+			</li>
+		</ul>
+		<?php
+				break;
+
+
+			default:
+				while($row=sql_fetch_array($query)) {
+					$job_row = sql_fetch("select * from alice_alba where `no`='".$row['wr_to']."'");
+					$info = $netfu_mjob->get_alba($job_row);
+
+					if($row['etc_4']){
+						$open_date = '<span><em>열람</em></span> <span class="reading_date">' . strtr(substr($row['etc_4'],0,11),'-','.') . '</span>';
+					} else {
+						if($type=='become_email'){
+							if(stristr($row['etc_1'],'email')){	// 이메일의 경우 이메일 확인
+								$open_date = "<span>이메일 확인</span>";
+							} else {
+								$open_date = "<span>열람전</span>";
+							}
+						} else {
+							$open_date = "<span>열람전</span>";
+						}
+					}
+		?>
+		<ul class="list_con">
+			<li class="col1"><input type="checkbox" name="chk[]" value="<?=$row['no'];?>"></li>
+			<li class="col2">
+				<div class="list_txt"><a href="../job/detail.php?no=<?=$row['wr_to'];?>"><?=stripslashes(strip_tags($job_row['wr_subject']));?></a></div>
+				<div class="list_etc">
+					<span>지원일 : <span class="join_date"><?=$netfu_util->get_date('dot', $row['wdate']);?></span></span>
+				</div>
+				<div class="list_etc">
+					<span><?=stripslashes(strip_Tags($job_row['wr_company_name']));?></span><span><?=$info['volume_text'];?></span>
+				</div>
+				<div class="list_etc">
+				<?=$open_date;?>
+				</div>
+			</li>
+		</ul>
+		<?php
+				}
+				break;
+		}
+		?>
+
+		<?=$paging;?>
+	</div>
+</section>
+
+<div class="button_con button_con3">
+	<a href="javascript:netfu_mjob.receive_all_delete()" class="bottom_btn03">삭제</a>
+</div>
+</form>
+
+<?php
+include "../include/tail.php";
+?>
